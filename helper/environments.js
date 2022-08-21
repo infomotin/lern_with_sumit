20d87ef2ca0ef8f11cc758656d9de1bb8fc2e02f
Abdul Motin <motin.mmk.jr@gmail.com>
2022-08-21 17:03:41 +0600

    hash and hash password end

--- helper/environments.js
+++ helper/environments.js
@@ -1,19 +1,24 @@
-const environments = {}
+const environments = {};
 
 environments.staging = {
-    port: 3001,
-    envName: 'staging'
-}
+  port: 3001,
+  envName: "staging",
+  secretKey: "secretKey",
+};
 
 environments.production = {
   port: 4001,
   envName: "production",
+  secretKey: "secretKey",
 };
 
 const currentEnvironment =
   typeof process.env.NODE_ENV === "string"
     ? process.env.NODE_ENV.toLowerCase()
     : "staging";
-const environmentsToExport = typeof environments[currentEnvironment] === "object"? environments[currentEnvironment]: environments.staging;
+const environmentsToExport =
+  typeof environments[currentEnvironment] === "object"
+    ? environments[currentEnvironment]
+    : environments.staging;
 
 module.exports = environmentsToExport;
--- helper/utilites.js
+++ helper/utilites.js
@@ -1,3 +1,5 @@
+const _crypto = require('crypto');
+const environments = require("./environments");
 const utilities = {}
 
 utilities.parseJSON = function (jsonData) {
@@ -10,7 +12,18 @@ utilities.parseJSON = function (jsonData) {
     }
     return output;
 }
-
+//hasing password
+utilities.hash = function (string) {
+    if (typeof string === 'string' && string.length > 0) {
+        const hash = _crypto
+          .createHmac("sha256", environments[process.env.NODE_ENV].secretKey)
+          .update(string)
+          .digest("hex");
+    } else {
+        return false;
+    }
+  
+};
 
 
 module.exports = utilities;
