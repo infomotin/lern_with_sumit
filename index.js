18d1c5512227ff118b206dd17f9f4ac83e9b0fa2
Abdul Motin <motin.mmk.jr@gmail.com>
2022-08-21 12:36:47 +0600

    fix error on chosePath

--- index.js
+++ index.js
@@ -21,23 +21,33 @@ const app = {};
 //   }
 // );
 
-// update file 
-data.update("test", "newFile", { "name": "Motin", "language": "Bangla" }, (err) => {
-  if (!err) {
-    console.log("file updated");
-  } else {
-    console.log(err);
-  }
-});
+// update file
+// data.update("test", "newFile", { "name": "Motin", "language": "Bangla" }, (err) => {
+//   if (!err) {
+//     console.log("file updated");
+//   } else {
+//     console.log(err);
+//   }
+// });
+//delete file
+// data.delete("test", "newFile", (err) => {
+//   if (!err) {
+//     console.log("file deleted");
+//   } else {
+//     console.log(err);
+//   }
+// });
+
+
 // data read data file
 
-data.read("test", "newFile", (err, data) => { 
-    if (!err && data) {
-    console.log(data);
-    } else {
-    console.log(err);
-    }
-});
+// data.read("test", "newFile", (err, data) => { 
+//     if (!err && data) {
+//     console.log(data);
+//     } else {
+//     console.log(err);
+//     }
+// });
 // create server
 app.createServer = function createServer() {
   const server = _http.createServer(app.handleReqRes);
